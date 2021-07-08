227683af5dac42ab3fd925484ce7597678212b57
s1280011 <s1280011@u-aizu.ac.jp>
2021-07-08 20:03:58 +0900

    Won/lost message added

--- excise5-1.c
+++ excise5-1.c
@@ -1,13 +1,15 @@
-
-
-
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 
 int main(){
   int a, b;
-
+  char name[10];
+  
+  printf("What is your name?\n>");
+  scanf("%s", &name);
+  printf("Hello, %s\n", name);
+  
   srand((unsigned int) time(NULL));
   a = rand() % 6 + 1;
   b = rand() % 6 + 1;
@@ -16,6 +18,8 @@ int main(){
   printf("Die 1: %d\n", a);
   printf("Die 2: %d\n", b);
   printf("Total value: %d\n", a + b);
+  if(a + b >= 7) printf("you won!");
+  else printf("You lost.");
   return 0;
 
 }
