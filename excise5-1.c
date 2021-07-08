5a20070e1e1307cd3ee8c534c90f43b7b6af3fa0
s1280011 <s1280011@u-aizu.ac.jp>
2021-07-08 19:43:00 +0900

    Greetings added

--- excise5-1.c
+++ excise5-1.c
@@ -1,9 +1,16 @@
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 
 int main(){
   int a, b;
+  char name[10];
+  
+  printf("What is your name?\n>");
+  scanf("%s", &name);
+  printf("Hello, %s\n", name);
+  
   srand((unsigned int) time(NULL));
   a = rand() % 6 + 1;
   b = rand() % 6 + 1;
